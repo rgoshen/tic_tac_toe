16dec493af9f83b52fbb232fcdc5305e4a4368cf
Richard Goshen <rick.goshen@gmail.com>
2023-05-13 12:54:00 -0700

    add game state to project

--- src/TicTacToe.tsx
+++ src/TicTacToe.tsx
@@ -2,24 +2,33 @@
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMS } from './constants';
-import { getRandomInt } from './utils';
+import {
+  DIMENSIONS,
+  PLAYER_X,
+  PLAYER_O,
+  SQUARE_DIMS,
+  GAME_STATES,
+} from './constants';
+import { getRandomInt, switchPlayer } from './utils';
 
 const emptyGrid = new Array(DIMENSIONS ** 2).fill(null);
 
 export default function TicTacToe() {
   const [grid, setGrid] = useState(emptyGrid);
-  const [players, setPlayers] = useState({
-    human: PLAYER_X,
-    ai: PLAYER_O,
+  const [players, setPlayers] = useState<Record<string, number | null>>({
+    human: null,
+    ai: null,
   });
+  const [gameState, setGameState] = useState(GAME_STATES.notStarted);
 
-  const move = (index: number, player: number) => {
-    setGrid((grid) => {
-      const gridCopy = grid.concat();
-      gridCopy[index] = player;
-      return gridCopy;
-    });
+  const move = (index: number, player: number | null) => {
+    if (player !== null) {
+      setGrid((grid) => {
+        const gridCopy = grid.concat();
+        gridCopy[index] = player;
+        return gridCopy;
+      });
+    }
   };
 
   const aiMove = () => {
@@ -36,7 +45,23 @@ export default function TicTacToe() {
     }
   };
 
-  return (
+  const choosePlayer = (option: number) => {
+    setPlayers({ human: option, ai: switchPlayer(option) });
+    setGameState(GAME_STATES.inProgress);
+  };
+
+  return gameState === GAME_STATES.notStarted ? (
+    <div>
+      <Inner>
+        <p>Choose your player</p>
+        <ButtonRow>
+          <button onClick={() => choosePlayer(PLAYER_X)}>X</button>
+          <p>or</p>
+          <button onClick={() => choosePlayer(PLAYER_O)}>O</button>
+        </ButtonRow>
+      </Inner>
+    </div>
+  ) : (
     <Container dims={DIMENSIONS}>
       {grid.map((value, index) => {
         const isActive = value !== null;
@@ -74,4 +99,17 @@ const Square = styled.div`
 
 const Marker = styled.p`
   font-size: 68px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  width: 150px;
+  justify-content: space-between;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
 `;
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,14 @@
 // utils.ts
 
+import { PLAYER_O, PLAYER_X } from './constants';
+
 // Get a random integer in a range min-max
 export const getRandomInt = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const switchPlayer = (player: number) => {
+  return player === PLAYER_X ? PLAYER_O : PLAYER_X;
 };
