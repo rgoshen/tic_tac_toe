e75f28b54955689dd3f7d91f8bebaec5a426c00c
Richard Goshen <rick.goshen@gmail.com>
2023-05-13 12:40:39 -0700

    add initial game board setup

---
+++ src/TicTacToe.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMS } from './constants';
+
+const emptyGrid = new Array(DIMENSIONS ** 2).fill(null);
+
+export default function TicTacToe() {
+  const [grid, setGrid] = useState(emptyGrid);
+  const [players, setPlayers] = useState({
+    human: PLAYER_X,
+    ai: PLAYER_O,
+  });
+
+  const move = (index: number, player: number) => {
+    setGrid((grid) => {
+      const gridCopy = grid.concat();
+      gridCopy[index] = player;
+      return gridCopy;
+    });
+  };
+
+  const humanMove = (index: number) => {
+    if (!grid[index]) {
+      move(index, players.human);
+    }
+  };
+
+  return (
+    <Container dims={DIMENSIONS}>
+      {grid.map((value, index) => {
+        const isActive = value !== null;
+
+        return (
+          <Square key={index} onClick={() => humanMove(index)}>
+            {isActive && <Marker>{value === PLAYER_X ? 'X' : 'O'}</Marker>}
+          </Square>
+        );
+      })}
+    </Container>
+  );
+}
+
+const Container = styled.div<{ dims: number }>`
+  display: flex;
+  justify-content: center;
+  width: ${({ dims }) => `${dims * (SQUARE_DIMS + 5)}px`};
+  flex-flow: wrap;
+  position: relative;
+`;
+
+const Square = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${SQUARE_DIMS}px;
+  height: ${SQUARE_DIMS}px;
+  border: 1px solid black;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Marker = styled.p`
+  font-size: 68px;
+`;
